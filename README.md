0b97cf1cd9a6dcaa060fe0ab0d8c768d8ce22532
Mark Komissarov <mkomissarov207@gmail.com>
2022-07-16 00:18:40 +0300

    add whatsapp support

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This scrapper shows you the price of [devman's main](https://dvmn.org/modules/) course.
 
+The program send a message about discount to you WhatsApp account.
--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import requests
-import os
+import pywhatkit
 import re
 from bs4 import BeautifulSoup
 from requests import get
 
 
-def main():
+def parse():
     url = 'https://dvmn.org/modules/'
     response = get(url)
     html_soup = BeautifulSoup(response.text, 'html.parser')
     cost_data = html_soup.find('div', class_="text-nowrap")
     rub = re.findall('(\d+)', cost_data.text)
     rub = int(''.join(map(str, rub)))
-    if rub == 14000:
-        print('There is no sale :(')
+    return rub == 14000
+
+
+def send_message():
+    mobile = 'ENTER YOUR NUMBER'
+    message_true = "There is a sale on the main Devman's course!"
+    message_false = "There is no sale :("
+    if parse() == True:
+        pywhatkit.sendwhatmsg_instantly(phone_no=mobile, message=message_false)
     else:
-        print("There is a sale on the main Devman's course!")
+        pywhatkit.sendwhatmsg_instantly(phone_no=mobile, message=message_true)
+
+
+def main():
+    send_message()
 
 
 if __name__ == '__main__':
